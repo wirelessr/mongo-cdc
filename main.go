c94cf9b6f97578e2b69ed4b76fcae70daf9dd14d
Chunting Wu <chunting.wu@langlive.com>
2024-11-08 18:26:57 +0800

    fix: miss readpref

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type KeySchema struct {
